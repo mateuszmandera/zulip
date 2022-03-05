517f4c81c2d5f4b1b2fa51b6036be5bde31a9806
Mateusz Mandera <mateusz.mandera@zulip.com>
2022-03-05 16:01:19 +0100

    models: Add UserProfile.uuid field and backfill migrations.

--- zerver/models.py
+++ zerver/models.py
@@ -1679,6 +1679,12 @@ class UserProfile(AbstractBaseUser, PermissionsMixin, UserBaseSettings):
     tos_version: Optional[str] = models.CharField(null=True, max_length=10)
     api_key: str = models.CharField(max_length=API_KEY_LENGTH)
 
+    # A UUID generated on user creation. Introduced primarily to
+    # provide a unique key for a user for the mobile push
+    # notifications bouncer that will not have collisions after doing
+    # a data export and then import.
+    uuid: Optional[UUID] = models.UUIDField(null=True, default=uuid4, unique=True)
+
     # Whether the user has access to server-level administrator pages, like /activity
     is_staff: bool = models.BooleanField(default=False)
 
