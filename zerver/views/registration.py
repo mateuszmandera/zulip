mateuszmandera/zulip

commit 74dd21c8fa132f0e55691fc05cba812348a11d55

register: Allow creating non-ldap users via social backends.
In configurations that use the ldap authentication backend and a social
backend, make it possible to create non-ldap users via the social backend.

zerver/views/registration.py
@@ -35,7 +35,7 @@ from zerver.views.auth import create_preregistration_user, redirect_and_log_into
 
 from zproject.backends import ldap_auth_enabled, password_auth_enabled, \
     ZulipLDAPExceptionNoMatchingLDAPUser, email_auth_enabled, ZulipLDAPAuthBackend, \
-    email_belongs_to_ldap
+    email_belongs_to_ldap, any_social_backend_enabled
 
 from confirmation.models import Confirmation, RealmCreationKey, ConfirmationKeyException, \
     validate_key, create_confirmation_link, get_object_from_key, \
@@ -280,24 +280,20 @@ def accounts_register(request: HttpRequest) -> HttpResponse:
                                         prereg_user=prereg_user,
                                         return_data=return_data)
             if user_profile is None:
-                can_use_different_backend = email_auth_enabled(realm)
+                can_use_different_backend = email_auth_enabled(realm) or any_social_backend_enabled(realm)
                 if settings.LDAP_APPEND_DOMAIN:
                     # In LDAP_APPEND_DOMAIN configurations, we don't allow making a non-ldap account
                     # if the email matches the ldap domain.
                     can_use_different_backend = can_use_different_backend and (
                         not email_belongs_to_ldap(realm, email))
                 if return_data.get("no_matching_ldap_user") and can_use_different_backend:
-                    # If both the LDAP and Email auth backends are
+                    # If both the LDAP and Email or Social auth backends are
                     # enabled, and there's no matching user in the LDAP
                     # directory then the intent is to create a user in the
                     # realm with their email outside the LDAP organization
                     # (with e.g. a password stored in the Zulip database,
                     # not LDAP).  So we fall through and create the new
                     # account.
-                    #
-                    # It's likely that we can extend this block to the
-                    # Google and GitHub auth backends with no code changes
-                    # other than here.
                     pass
                 else:
                     # TODO: This probably isn't going to give a

zerver/tests/test_auth_backends.py
@@ -996,6 +996,38 @@ class SocialAuthBase(ZulipTestCase):
             self.stage_two_of_registration(result, realm, subdomain, email, name, name,
                                            skip_registration_form=self.BACKEND_CLASS.full_name_validated)
 
+    @override_settings(TERMS_OF_SERVICE=None)
+    def test_social_auth_with_ldap_auth_registration_from_confirmation(self) -> None:
+        """
+        This test checks that in configurations that use the ldap authentication backend
+        and a social backend, it is possible to create non-ldap users via the social backend.
+        """
+        self.init_default_ldap_database()
+        email = self.nonreg_email("alice")
+        name = "Alice Social"
+        realm = get_realm("zulip")
+        subdomain = "zulip"
+        ldap_user_attr_map = {'full_name': 'cn'}
+        account_data_dict = self.get_account_data_dict(email=email, name=name)
+
+        backend_path = 'zproject.backends.{}'.format(self.BACKEND_CLASS.__name__)
+        with self.settings(
+                POPULATE_PROFILE_VIA_LDAP=True,
+                LDAP_EMAIL_ATTR='mail',
+                AUTH_LDAP_USER_ATTR_MAP=ldap_user_attr_map,
+                AUTHENTICATION_BACKENDS=(backend_path,
+                                         'zproject.backends.ZulipLDAPAuthBackend',
+                                         'zproject.backends.ZulipDummyBackend')
+        ):
+            account_data_dict = self.get_account_data_dict(email=email, name=name)
+            result = self.social_auth_test(account_data_dict,
+                                           expect_choose_email_screen=True,
+                                           subdomain=subdomain, is_signup='1')
+            # Full name should get populated as provided by the social backend, because
+            # this user isn't in the ldap dictionary:
+            self.stage_two_of_registration(result, realm, subdomain, email, name, name,
+                                           skip_registration_form=self.BACKEND_CLASS.full_name_validated)
+
     def test_social_auth_complete(self) -> None:
         with mock.patch('social_core.backends.oauth.BaseOAuth2.process_error',
                         side_effect=AuthFailed('Not found')):

commit 4e6adf6677310874d6414b60eb2e51997c959efe

auth: Prevent unauthorized signup in ldap + external auth cases.
If:
- ZulipLDAPAuthBackend and an external authentication backend (any aside
  of ZulipLDAPAuthBackend and EmailAuthBackend) are the only ones
  enabled in AUTHENTICATION_BACKENDS in /etc/zulip/settings.py
- The organization permissions don't require invitations to join

An attacker can create a new account in the organization with an
arbitrary email address in their control that's not in the
organization's LDAP directory.

The impact is limited to installations which have this specific
combination of authentication backends as described above in addition to
having Invitations are required for joining this organization
organization permission disabled.

Bug introduced in 74dd21c8fa132f0e55691fc05cba812348a11d55

zerver/views/registration.py
@@ -418,9 +418,32 @@ def registration_helper(
                 return_data=return_data,
             )
             if user is None:
-                can_use_different_backend = email_auth_enabled(realm) or (
-                    len(get_external_method_dicts(realm)) > 0
-                )
+                # This logic is security-sensitive. The user has NOT been successfully authenticated
+                # with LDAP and we need to carefully decide whether they should be permitted to proceed
+                # with account creation anyway or be stopped. There are three scenarios to consider:
+                #
+                # 1. EmailAuthBackend is enabled for the realm. That explicitly means that a user
+                #    with a valid confirmation link should be able to create an account, because
+                #    they were invited or organization permissions allowed sign up.
+                # 2. EmailAuthBackend is disabled - that means the organization wants to be authenticating
+                #    users with an external source (LDAP or one of the ExternalAuthMethods). If the user
+                #    came here through one of the ExternalAuthMethods, their identity can be considered
+                #    verified and account creation can proceed.
+                # 3. EmailAuthBackend is disabled and the user did not come here through an ExternalAuthMethod.
+                #    That means they came here by entering their email address on the registration page
+                #    and clicking the confirmation link received. That means their identity needs to be
+                #    verified with LDAP - and that has just failed above. Thus the account should NOT be
+                #    created.
+                #
+                if email_auth_enabled(realm):
+                    can_use_different_backend = True
+                # We can identify the user came here through an ExternalAuthMethod by password_required
+                # being set to False on the PreregistrationUser object.
+                elif len(get_external_method_dicts(realm)) > 0 and not password_required:
+                    can_use_different_backend = True
+                else:
+                    can_use_different_backend = False
+
                 if settings.LDAP_APPEND_DOMAIN:
                     # In LDAP_APPEND_DOMAIN configurations, we don't allow making a non-LDAP account
                     # if the email matches the ldap domain.

zerver/tests/test_signup.py
@@ -2944,6 +2944,74 @@ class UserSignUpTest(ZulipTestCase):
 
     @override_settings(
         AUTHENTICATION_BACKENDS=(
+            "zproject.backends.SAMLAuthBackend",
+            "zproject.backends.ZulipLDAPAuthBackend",
+            "zproject.backends.ZulipDummyBackend",
+        )
+    )
+    def test_ldap_registration_email_backend_disabled_bypass_attempt(self) -> None:
+        """
+        Tests for the case of LDAP + external auth backend being the ones enabled and
+        a user using the registration page to get a confirmation link and then trying
+        to use it to create a new account with their own email that's not authenticated
+        by either of the backends.
+        """
+        email = "no_such_user_in_ldap@example.com"
+        subdomain = "zulip"
+
+        self.init_default_ldap_database()
+        ldap_user_attr_map = {"full_name": "cn"}
+        full_name = "New LDAP fullname"
+
+        result = self.client_post("/register/", {"email": email}, subdomain=subdomain)
+
+        self.assertEqual(result.status_code, 302)
+        self.assertTrue(
+            result["Location"].endswith(
+                f"/accounts/send_confirm/?email={urllib.parse.quote(email)}"
+            )
+        )
+        result = self.client_get(result["Location"])
+        self.assert_in_response("Check your email", result)
+
+        with self.settings(
+            POPULATE_PROFILE_VIA_LDAP=True,
+            LDAP_APPEND_DOMAIN="zulip.com",
+            AUTH_LDAP_USER_ATTR_MAP=ldap_user_attr_map,
+        ):
+            # Click confirmation link
+            result = self.submit_reg_form_for_user(
+                email,
+                None,
+                full_name="Ignore",
+                from_confirmation="1",
+                # Pass HTTP_HOST for the target subdomain
+                HTTP_HOST=subdomain + ".testserver",
+            )
+
+            self.assert_in_success_response(
+                ["We just need you to do one last thing.", email], result
+            )
+
+            # Submit the final form, attempting to register the user despite
+            # no match in ldap.
+            result = self.submit_reg_form_for_user(
+                email,
+                "newpassword",
+                full_name=full_name,
+                # Pass HTTP_HOST for the target subdomain
+                HTTP_HOST=subdomain + ".testserver",
+            )
+            # Didn't create an account
+            with self.assertRaises(UserProfile.DoesNotExist):
+                UserProfile.objects.get(delivery_email=email)
+            self.assertEqual(result.status_code, 302)
+            self.assertEqual(
+                result["Location"], "/accounts/login/?email=no_such_user_in_ldap%40example.com"
+            )
+
+    @override_settings(
+        AUTHENTICATION_BACKENDS=(
             "zproject.backends.ZulipLDAPAuthBackend",
             "zproject.backends.ZulipDummyBackend",
         )